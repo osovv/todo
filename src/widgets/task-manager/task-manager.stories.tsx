54a75799f99688a5bcf94660afe716db47bff0fc
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2022-11-07 17:47:20 +0300

    Create TaskManager widget

---
+++ src/widgets/task-manager/task-manager.stories.tsx
@@ -0,0 +1,38 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { fork } from 'effector';
+import { Provider } from 'effector-react/scope';
+import { $tasks } from '~/entities/task/model';
+import { TaskManager } from './task-manager';
+
+export default {
+  title: 'Widgets/TaskManager',
+  component: TaskManager,
+  decorators: [
+    (storyFn) => {
+      const scope = fork({
+        values: [
+          [
+            $tasks,
+            [
+              {
+                id: 1,
+                status: 'active',
+                title: 'Make a leather wallet',
+                description: 'Check YouTube for tutorial',
+              },
+            ],
+          ],
+        ],
+      });
+
+      return <Provider value={scope}>{storyFn()}</Provider>;
+    },
+  ],
+} as ComponentMeta<typeof TaskManager>;
+
+const Story: ComponentStory<typeof TaskManager> = (args) => {
+  return <TaskManager {...args} id={1} />;
+};
+
+export const Default = Story.bind({});
+Default.args = {};
